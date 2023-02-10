4162035c51be6705df9a6614aac63f586c196ae2
Delta Null <47459550+Null-Delta@users.noreply.github.com>
2023-02-11 00:53:24 +0300

    lab1 task2 method3

--- lab1/task2/methods.rb
+++ lab1/task2/methods.rb
@@ -23,7 +23,7 @@ def count_of_eval(value)
     str_value = value.to_s
     (0...str_value.size)
     .reduce(0) { |count, index|
-        if str_value[index].to_i > 3
+        if str_value[index].to_i > 3 && str_value[index].to_i % 2 == 1
             count + 1
         else 
             count
@@ -31,4 +31,28 @@ def count_of_eval(value)
     }
 end
 
-puts count_of_eval(123456789)
+#method 3
+
+def nums_sum(value)
+    str_value = value.to_s
+    (0...str_value.size)
+    .map { |index| str_value[index].to_i }
+    .reduce(&:+)
+end
+
+def find_solution(value)
+    value_sum = nums_sum(value)
+
+    (2...value)
+    .reduce(1) { |mult, divider|
+        if value % divider == 0 && nums_sum(divider) < value_sum
+            mult * divider
+        else
+            mult
+        end
+    }
+end
+
+puts easy_dividers_sum(99)
+puts count_of_eval(99)
+puts find_solution(99)
