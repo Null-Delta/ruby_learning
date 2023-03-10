309dcde009929e17f821e6dc866964b084375cb5
Delta Null <47459550+Null-Delta@users.noreply.github.com>
2023-03-10 23:42:16 +0300

    lab2_task1_part2

--- lab2/Student.rb
+++ lab2/Student.rb
@@ -82,4 +82,27 @@ class Student
     def set_git(new_git)
         @git = new_git
     end
+
+    def to_s
+        if @id != nil
+            id_info = "ID: #{@id}"
+        end
+
+        fio_info = "ФИО: #{@lastname} #{@firstname} #{@patronymic}"
+
+        if have_any_contact
+            contacts_info = "Контакты:"
+
+            contacts_info += @phone == nil ? "" : "\n\tТелефон: #{@phone}"
+            contacts_info += @telegram == nil ? "" : "\n\tТелеграм: #{@telegram}"
+            contacts_info += @email == nil ? "" : "\n\tПочта: #{@email}"
+            contacts_info += @git == nil ? "" :  "\n\tГит: #{@git}"
+        end
+
+        [id_info, fio_info, contacts_info].join("\n")
+    end
+
+    def have_any_contact
+        @phone != nil || @telegram  != nil || @email != nil || @git != nil
+    end
 end
---
+++ lab2/main.rb
@@ -0,0 +1,7 @@
+require_relative './Student.rb'
+
+student1 = Student.new("Хахук", "Рустам", "Нальбиевич", "2356", nil, "@delta_null", "delta.null@vk.ru", nil)
+student2 = Student.new("Шичиях", "Ильяс", "Рашидович")
+
+puts student1
+puts student2
