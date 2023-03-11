b35fd957a1035b0b4007e761ef8acc25d92fe74e
Delta Null <47459550+Null-Delta@users.noreply.github.com>
2023-03-11 09:37:38 +0300

    lab2_task2_part10

--- lab2/main.rb
+++ lab2/main.rb
@@ -7,4 +7,19 @@ def read_from_txt(filename)
     .map { |v| Student.from_string(v) }
 end
 
-puts read_from_txt("dataset.txt")
+def write_to_txt(filename, students)
+    File.open(filename, 'w') { |file| 
+        file.write(
+            students.map { |student| 
+                student.get_info
+            }
+            .join("\n")
+        )
+    }
+end
+
+array = read_from_txt("dataset.txt")
+
+array[0].set_contacts(email: "updated.mail@email.com")
+write_to_txt("new_dataset.txt", array)
+array = read_from_txt("new_dataset.txt")
