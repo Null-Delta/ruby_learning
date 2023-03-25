8a14d4e8b286c991a3c99e42fe4a41c9a9d19913
Delta Null <47459550+Null-Delta@users.noreply.github.com>
2023-03-25 08:26:56 +0300

    lab2 task3 subtask4

--- lab2/data_list.rb
+++ lab2/data_list.rb
@@ -1,7 +1,25 @@
 class DataList 
     attr_private_accessor :list
+    attr_private_accessor :selected_objects
 
     def initialize(list:)
         self.list = list
+    end
+
+    def select(index)
+        selected_objects << index
+    end
+
+    def get_selected_objects
+        (0..<list.size)
+            .filter { |x| selected_objects.include? x }
+    end
+
+    def get_names
+        raise "Метод не реализован"
+    end
+
+    def get_data
+        raise "Метод не реализован"
     end
 end
